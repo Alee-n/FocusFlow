c0c90dba6ee1a220d474b8b2767f51534224434d
Lia <liaaleen22@gmail.com>
2026-06-06 15:48:30 +0530

    Finalize FocusFlow project 2.1

--- database/db.py
+++ database/db.py
@@ -3,6 +3,11 @@ import os
 
 def connect_db():
 
+    print("DB_HOST =", os.getenv("DB_HOST"))
+    print("DB_NAME =", os.getenv("DB_NAME"))
+    print("DB_USER =", os.getenv("DB_USER"))
+    print("DB_PORT =", os.getenv("DB_PORT"))
+
     connection = psycopg2.connect(
         host=os.getenv("DB_HOST"),
         database=os.getenv("DB_NAME"),
