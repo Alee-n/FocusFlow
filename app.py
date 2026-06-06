df96c020b07ea9da449b6e431da3ccf323704613
Lia <liaaleen22@gmail.com>
2026-06-06 16:21:29 +0530

    Fix CI database initialization

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ app.register_blueprint(main)
 app.register_blueprint(auth)
 app.register_blueprint(api)
 
-create_tables()
+if os.getenv("RENDER"):
+    create_tables()
 
 if __name__ == "__main__":
     app.run(debug=True)
